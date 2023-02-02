764348142248b36f47f3683eb370840ae0580695
oanhpham <phamoanh22365@gmail.com>
2023-02-02 20:45:27 +0700

    eleven

---
+++ index.js
@@ -0,0 +1,14 @@
+let slider= document.getElementById("slidebar");
+let bars= document.getElementById("check");
+
+function OpenSlider(){
+    slider.style.display="block";
+    
+    
+
+}
+
+function CloseSlider(){
+    slider.style.display="none";
+   
+}
